d4837cada73e1c9060b9f33331743d3cd7707f35
Vinicius Becaleti <vinicius@becaleti.com>
2023-12-23 20:45:33 -0300

    ♻️ refactor: altera a consulta da transação única para pegar apenas o primeiro resultado

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -5,7 +5,26 @@ import { z } from 'zod'
 export async function transactionsRoutes(app: FastifyInstance) {
   app.get('/', async (request: FastifyRequest, reply: FastifyReply) => {
     const transactions = await knex('transactions').select('*')
-    return reply.send(transactions)
+    return reply.send({
+      transactions,
+    })
+  })
+
+  app.get('/:id', async (request: FastifyRequest, reply: FastifyReply) => {
+    const getTransactionParamsSchema = z.object({
+      id: z.string().uuid(),
+    })
+
+    const { id } = getTransactionParamsSchema.parse(request.params)
+
+    const transaction = await knex('transactions')
+      .select('*')
+      .where({ id })
+      .first()
+
+    return reply.send({
+      transaction,
+    })
   })
 
   app.post('/', async (request: FastifyRequest, reply: FastifyReply) => {
